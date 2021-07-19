9e518277d0a294ec44bc418706be72a460d77cd2
Strumber_W10 <Strumber_W10@Strumber_W10-PC>
2021-07-19 17:54:55 +0200

    Commit Inicial

---
+++ src/main/java/com/example/M12_nivell3/M12Nivell3Application.java
@@ -0,0 +1,13 @@
+package com.example.M12_nivell3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M12Nivell3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M12Nivell3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/M12_nivell3/M12Nivell3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.M12_nivell3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M12Nivell3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
