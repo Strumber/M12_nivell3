8ebc8b6986a3771eee2f0f11ab35bc93d6943ad2
Strumber_W10 <Strumber_W10@Strumber_W10-PC>
2021-07-20 10:21:53 +0200

    LLiurament m12_nivell3

--- src/main/java/com/example/M12_nivell3/M12Nivell3Application.java
+++ src/main/java/com/example/M12_nivell3/M12Nivell3Application.java
@@ -1,13 +1,19 @@
 package com.example.M12_nivell3;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import springBootInitialDemo.App;
+
+
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class M12Nivell3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(M12Nivell3Application.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
